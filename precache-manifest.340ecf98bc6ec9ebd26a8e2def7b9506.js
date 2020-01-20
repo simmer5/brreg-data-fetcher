23c648a7ba5ca9c5ba372ab23d5786901f0cb14c
Mindaugas <strumke@yahoo.com>
2020-01-20 22:15:55 +0100

    Updates

---
+++ precache-manifest.340ecf98bc6ec9ebd26a8e2def7b9506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484e18353d942714c005fd5edaef225d",
+    "url": "/brreg-data-fetcher/index.html"
+  },
+  {
+    "revision": "fe595fb48f344894c484",
+    "url": "/brreg-data-fetcher/static/css/main.522c7d37.chunk.css"
+  },
+  {
+    "revision": "6626cf6479cffa24b86c",
+    "url": "/brreg-data-fetcher/static/js/2.5707810f.chunk.js"
+  },
+  {
+    "revision": "fe595fb48f344894c484",
+    "url": "/brreg-data-fetcher/static/js/main.a4d6ada4.chunk.js"
+  },
+  {
+    "revision": "07b59ade70759ada5905",
+    "url": "/brreg-data-fetcher/static/js/runtime-main.1bcab204.js"
+  }
+]);
